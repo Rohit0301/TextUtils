9be9661486bfae17a52e7cafb0f3182a309e04bf
Rohit0301 <rj03012002@gmail.com>
2020-12-03 23:17:44 +0530

    added bootstap and some new features

--- TextUtils/views.py
+++ TextUtils/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
-    return render(request,'index.html')
+    return render(request,'homepage.html')
     # return HttpResponse('Home')
 
 def Analyze(request):
+    #get the text
     djtext=request.GET.get('text','default')
+
+    #checkboxes values
     removepunc=request.GET.get('removepunc','off')
-    print(removepunc)
+    fullcaps=request.GET.get('fullcaps','off')
+    newlineremover=request.GET.get('newlineremover','off')
+    extraspaceremover=request.GET.get('extraspaceremover','off')
+    charcounter=request.GET.get('charcounter','off')
+
+    #check which checkbox is selected
     if removepunc=="on":
         analyzed=""
         Punctuations='''!()-[]{}:;'"\,<>./?@#$%^&*_~'''
@@ -16,5 +24,38 @@ def Analyze(request):
                 analyzed=analyzed+char
         params={'purpose':'Removed Punctuation','analyzed_text':analyzed}
         return render(request,'analyze.html',params)
+    elif fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params={'purpose':'Changed to uppercase','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params={'purpose':'Removed newLine','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif extraspaceremover=="on":
+        analyzed=""
+        for index,char in enumerate(djtext):
+            if not(djtext[index]==" " and djtext[index+1]==" "):
+                analyzed=analyzed+djtext[index]
+                
+        params={'purpose':'Removed Extra space','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
+    elif charcounter=="on":
+        counter=0;
+        for char in djtext:
+            counter=counter+1;
+        analyzed="Total characters are {}".format(counter) 
+                
+        params={'purpose':'Characters Counted','analyzed_text':analyzed}
+        return render(request,'analyze.html',params)
+
     else :
         return HttpResponse('Error')
